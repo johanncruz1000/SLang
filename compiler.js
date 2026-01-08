52f43324b5a4c7092624bc0be6e21c5aa3ba9284
Johann-SLang <JohannCruz1000@gmail.com>
2026-01-08 15:24:36 -0300

    Adição do compilador em bash

--- compiler.js
+++ compiler.js
@@ -1,10 +1,12 @@
 const {exec}=require('child_process')
-const args=process.argv.slice(2);
+const {promisify}=require("util");
+const execAsync=promisify(exec);
+const args=process.argv;
 const fs=require("fs");
-var code=fs.readFileSync(args[0],'utf-8')
+var code=fs.readFileSync(args[2],'utf-8')
 
 code=`void print(std::string a){std::cout << a;} std::string input(){std::string a; std::getline(std::cin,a); return a;} int Int(std::string a){return std::stoi(a);} int main(){ \n${code}\n}`
-if(code.includes("print")) code=`#include <iostream>\n#include <string>\n#using string=std::string;\n ${code}`
+if(code.includes("print")) code=`#include <iostream>\n#include <string>\nusing string=std::string;\n ${code}`
 code=code.split("\n").join(";\n").replace(/;;/g,";").replace(/  /g,"").replace(/};/g,"}").replace(/{;/g,"{").replace(/{ ;/g,"{").replace(/\n;/g,"\n").replace(/>;/g,">")
 //console.log(code)
 function detect(codigo) {
@@ -19,6 +21,11 @@ code=code.replace(`var ${nome}`,`string ${nome}`)
 }
 detect(code)
 
-exec(`cat > ${args[0].replace(".sl",".cpp")} << 'EOF'\n${code}\nEOF`)
-exec(`clang++ ${args[0].replace(".sl",".cpp")} -O3 -o ${args[1]}`)
+exec(`cat > ${args[2].replace(".sl",".cpp")} << 'EOF'\n${code}\nEOF`)
+execAsync(`clang++ ${args[2].replace(".sl",".cpp")} -O3 -o ${args[3]}`).then(()=>{
+if(args[4]=="-emit-cpp"){
+}else{
+exec(`rm ${args[2].replace(".sl",".cpp")}`)
+}
+})
 //*/
