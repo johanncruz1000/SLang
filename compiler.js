f8e553d5b20c187dec9e691c8ec739c9c7706b0c
Johann-SLang <JohannCruz1000@gmail.com>
2026-01-08 12:26:21 -0300

    Adição de variaveis automaticas

--- compiler.js
+++ compiler.js
@@ -3,17 +3,17 @@ const args=process.argv.slice(2);
 const fs=require("fs");
 var code=fs.readFileSync(args[0],'utf-8')
 
-code=`void print(std::string a){std::cout << a;} std::string input(){std::string a; std::getline(std::cin,a); return a;} int Int(std::string a){return std::stoi(a);} int main(){ ${code} }`
-if(code.includes("print")) code=`#include <iostream>\n#include <string>\n#include <any>\n ${code}`
+code=`void print(std::string a){std::cout << a;} std::string input(){std::string a; std::getline(std::cin,a); return a;} int Int(std::string a){return std::stoi(a);} int main(){ \n${code}\n}`
+if(code.includes("print")) code=`#include <iostream>\n#include <string>\n#using string=std::string;\n ${code}`
 code=code.split("\n").join(";\n").replace(/;;/g,";").replace(/  /g,"").replace(/};/g,"}").replace(/{;/g,"{").replace(/{ ;/g,"{").replace(/\n;/g,"\n").replace(/>;/g,">")
-console.log(code)
+//console.log(code)
 function detect(codigo) {
-    return codigo.replace(/^var\s+(\w+)\s*=\s*(.+?)(?=;|\n|$)/gm, (match, nome, valor) => {
-console.log(valor);
-if(/^-?\d+$/g.test(valor) && !valor.includes(`"`)){console.log(`${nome} é int com o valor ${valor}`)}
-if(/(-?\d*\.\d+)/g.test(valor) && !valor.includes(`"`)){console.log(`${nome} é double com valor de ${valor}`)}
-if(valor == "true" || valor == "false"){console.log(`${nome} é bool com o valor ${valor}`)}
-if(valor.includes(`"`)) {console.log(nome+" é string com valor "+ valor);
+    return codigo.replace(/(?<!["\/])var\s+(\w+)\s*=\s*(.+?)(?=;|\n|$)/gm, (match, nome, valor) => {
+if(/^-?\d+$/g.test(valor) && !valor.includes(`"`)){code=code.replace(`var ${nome}`,`int ${nome}`); console.log(code)}
+if(/(-?\d*\.\d+)/g.test(valor) && !valor.includes(`"`)){code=code.replace(`var ${nome}`,`double ${nome}`)}
+if(valor == "true" || valor == "false"){code=code.replace(`var ${nome}`,`bool ${nome}`)}
+if(valor.includes(`"`)) {
+code=code.replace(`var ${nome}`,`string ${nome}`)
  }
 });
 }
