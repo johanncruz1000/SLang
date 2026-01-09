83f3d4f6bedd28c5ac50a920b194fe608a4e535f
Johann-SLang <JohannCruz1000@gmail.com>
2026-01-09 12:40:40 -0300

    Adição de funçoes,whiles,ifs e elses

--- compiler.js
+++ compiler.js
@@ -3,12 +3,25 @@ const {promisify}=require("util");
 const execAsync=promisify(exec);
 const args=process.argv;
 const fs=require("fs");
+const path=require("path");
 var code=fs.readFileSync(args[2],'utf-8')
-
-code=`void print(std::string a){std::cout << a;} std::string input(){std::string a; std::getline(std::cin,a); return a;} int Int(std::string a){return std::stoi(a);} int main(){ \n${code}\n}`
-if(code.includes("print")) code=`#include <iostream>\n#include <string>\nusing string=std::string;\n ${code}`
+var setup=fs.readFileSync(path.join(__dirname,"setup.cpp"),'utf8')
+code=`void print(std::string a){std::cout << a;} std::string input(){std::string a; std::getline(std::cin,a); return a;} int Int(std::string a){return std::stoi(a);} \nint main(){ \n${code}\nreturn 0;\n}`
+code=setup+code;
 code=code.split("\n").join(";\n").replace(/;;/g,";").replace(/  /g,"").replace(/};/g,"}").replace(/{;/g,"{").replace(/{ ;/g,"{").replace(/\n;/g,"\n").replace(/>;/g,">")
-//console.log(code)
+code = code.replace(/(if|while|else\s+if|else)\s*(?:\((.*?)\))?\s*\{(.*?)\}/gs, 
+    (match, tipo, condicao, conteudo) => {
+        if (tipo === "else") {
+            return `else OPENING_BRACE ${conteudo} CLOSING_BRACE`;
+        } else {
+            return `${tipo}(${condicao}) OPENING_BRACE ${conteudo} CLOSING_BRACE`;
+        }
+    }
+);
+code=code.replace(/int\s+main\((.*?)\){(.*?)func\s+(.*?)\s+((.*?))\((.*?)\)\{(.*?)\}(.*?)\}/gs,"$3 $4($6){$7} int main($1){$2 $8}");
+code=code.replace(/CLOSING_BRACE/g,"}")
+code=code.replace(/OPENING_BRACE/g,"{")
+console.log(code)
 function detect(codigo) {
     return codigo.replace(/(?<!["\/])var\s+(\w+)\s*=\s*(.+?)(?=;|\n|$)/gm, (match, nome, valor) => {
 if(/^-?\d+$/g.test(valor) && !valor.includes(`"`)){code=code.replace(`var ${nome}`,`int ${nome}`); console.log(code)}
---
+++ setup.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+template<typename T>std::string String(T a){
+return std::to_string(a);
+}
