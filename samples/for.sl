4aa3b01bc1c0c84945b82a99a39718d8ae34226a
Johann-SLang <JohannCruz1000@gmail.com>
2026-01-09 13:05:02 -0300

    Adição de for

---
+++ samples/for.sl
@@ -0,0 +1,7 @@
+func void count(){
+for(var i=0; i<10;i++){
+print("numero atual:" + String(i))
+print("\n")
+}
+}
+count()
--- setup.cpp
+++ setup.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+void printl(std::string a){
+std::cout << a << std::endl;
+}
 template<typename T>std::string String(T a){
 return std::to_string(a);
 }
