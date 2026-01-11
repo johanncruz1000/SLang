b7d08dc7d990f106b318fe68dc8d5d972c18fbd6
Johann-SLang <JohannCruz1000@gmail.com>
2026-01-11 20:55:22 -0300

    Var melhorado

--- compiler.js
+++ compiler.js
@@ -6,6 +6,7 @@ const fs=require("fs");
 const path=require("path");
 var code=fs.readFileSync(args[2],'utf-8')
 var setup=fs.readFileSync(path.join(__dirname,"setup.cpp"),'utf8')
+var variaveis=[];
 code.replace(/import\s*(<|")(.*?)\.(.*?)(>|")/g,(match,start,name,ext,end)=>{
 code=code.replace(`import ${start}${name}.${ext}${end}`,``)
 if(ext=="sl"){
@@ -15,9 +16,7 @@ code=`#include ${start}${name}${ext}${end}\n${code}`
 }
 })
 code=`std::string input(){std::string a; std::getline(std::cin,a); return a;} int Int(std::string a){return std::stoi(a);} \nint main(){ \n${code}\nreturn 0;\n}`
-code=setup+code;
-code=code.split("\n").join(";\n").replace(/;;/g,";").replace(/  /g,"").replace(/};/g,"}").replace(/{;/g,"{").replace(/{ ;/g,"{").replace(/\n;/g,"\n").replace(/>;/g,">")
-
+code=code.split("\n").join(";\n").replace(/;;/g,";").replace(/  /g,"").replace(/(for|if|while|else\s+if|else)\s*(.*?)};/g,"$1 $2 }").replace(/{;/g,"{").replace(/{ ;/g,"{").replace(/\n;/g,"\n").replace(/#include\s+<(.*?)>;/g,"#include <$1>").replace(/template(.*?)>;/g,"template $1>").replace(/:;/g,":")
 code = code.replace(/(for|if|while|else\s+if|else)\s*(?:\((.*?)\))?\s*\{(.*?)\}/gs, 
     (match, tipo, condicao, conteudo) => {
         if (tipo === "else") {
@@ -30,21 +29,26 @@ code = code.replace(/(for|if|while|else\s+if|else)\s*(?:\((.*?)\))?\s*\{(.*?)\}/
 code=code.replace(/int\s+main\((.*?)\){(.*?)func\s+(.*?)\s+((.*?))\((.*?)\)\{(.*?)\}(.*?)\}/gs,"$3 $4($6){$7} int main($1){$2 $8}");
 code=code.replace(/CLOSING_BRACE/g,"}")
 code=code.replace(/OPENING_BRACE/g,"{")
-console.log(code)
+//console.log(code)
 function detect(codigo) {
-    return codigo.replace(/(?<!["\/])var\s+(\w+)\s*=\s*(.+?)(?=;|\n|$)/gm, (match, nome, valor) => {
-if(/^-?\d+$/g.test(valor) && !valor.includes(`"`)){code=code.replace(`var ${nome}`,`int ${nome}`); console.log(code)}
-if(/(-?\d*\.\d+)/g.test(valor) && !valor.includes(`"`)){code=code.replace(`var ${nome}`,`double ${nome}`)}
-if(valor == "true" || valor == "false"){code=code.replace(`var ${nome}`,`bool ${nome}`)}
-if(valor.includes(`"`)) {
-code=code.replace(`var ${nome}`,`string ${nome}`)
- }
+    return codigo.replace(/(?<!["\/]|\busing\b\s+)var\s+(\w+)\s*=\s*(.+?)(?=;|\n|$)/gm, (match, nome, valor) => {
+variaveis.push(nome);
+//getVar();
+})
+}
+function getVar(){
+for (const nome of variaveis) {
+const regex = new RegExp(`(?<!var\\s+)\\b${nome}\\b(?!\\s*=)`, 'g');
+code = code.replace(regex, (match) => {
+return `std::visit(VARVISITANTE{},${nome})`; 
 });
+    }
 }
 detect(code)
-
+code=setup+code;
+console.log(variaveis)
 exec(`cat > ${args[2].replace(".sl",".cpp")} << 'EOF'\n${code}\nEOF`)
-execAsync(`clang++ -ffunction-sections -fdata-sections ${args[2].replace(".sl",".cpp")} -O3 -o ${args[3]} -Wl,--gc-sections`).then(()=>{
+/*execAsync(`clang++ ${args[2].replace(".sl",".cpp")} -O0 -o ${args[3]}`).then(()=>{
 if(args[4]=="-emit-cpp"){
 }else{
 exec(`rm ${args[2].replace(".sl",".cpp")}`)
--- setup.cpp
+++ setup.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <filesystem>
+#include <regex>
 #include <cstdlib>
-void print(std::string a){
+#include <sstream>
+class var{
+private:
+using VAR1_NAME = std::variant<int, double, std::string, bool>;
+VAR1_NAME value;
+struct TOINTVAR {
+int operator()(int x) { return x; }
+int operator()(double x) { return static_cast<int>(x); }
+int operator()(const std::string& x) {
+return x.size();
+}
+int operator()(bool x) {
+return x ? 1 : 0;
+}
+};
+public:
+var() : value(0){}
+var(int x) : value(x){}
+int ToInt() {
+return std::visit(TOINTVAR{},value);
+}
+operator int() {
+return ToInt();
+}
+bool operator<(int limite){
+return ToInt()<limite;
+}
+var& operator++(){
+if(std::holds_alternative<int>(value)){
+int& val=std::get<int>(value);
+val+=1;
+ }
+return *this;
+}
+var operator++(int){
+var temp = *this;
+++(*this);
+return temp;
+}
+var operator+=(int valor){
+if(std::holds_alternative<int>(value)){
+int& val=std::get<int>(value);
+val+=valor;
+  }
+return *this;
+ }
+};
+template<typename... Args>
+void print(Args&&... args){
+std::ostringstream value;
+value << std::boolalpha;
+(value << ... << std::forward<Args>(args));
+std::string a = value.str();
 std::cout << a << std::endl;
 }
 template<typename T>std::string String(T a){
-return std::to_string(a);
+std::ostringstream value;
+value << a;
+return value.str();
 }
